24244cdf0e9f322cdebb6003036ff0d109f964b7
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-10-18 03:20:55 +0100

    Update: Great addtions! Now I have a new look to my portfolio webite!

---
+++ script.js
@@ -0,0 +1,8 @@
+const getData = () => {
+  const name = document.getElementById('name').value;
+  console.log(name);
+  const email = document.getElementById('email').value;
+  console.log(email);
+  const message = document.getElementById('message').value;
+  console.log(message);
+}
