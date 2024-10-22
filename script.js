308e58a23098c481c8a1a1b1033aa6d238c29438
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-10-22 05:02:29 +0100

    Goodnight's work. 5am, 22nd Octobeer 2024.

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const getData = () => {
-  const name = document.getElementById('name').value;
-  console.log(name);
-  const email = document.getElementById('email').value;
-  console.log(email);
-  const message = document.getElementById('message').value;
-  console.log(message);
-}
+$('.services-carousel').owlCarousel({
+  autoplay: true,
+  loop: true,
+  margin: 20,
+  dots: true,
+  nav: false,
+  responsiveClass: true,
+  responsive: { 
+    0: { 
+      items: 1
+    },
+    768: { 
+      items: 2
+    },
+    900: {
+      items: 4
+    }
+  }
+});
+
+
+
+
+// const getData = () => {
+//   const name = document.getElementById('name').value;
+//   console.log(name);
+//   const email = document.getElementById('email').value;
+//   console.log(email);
+//   const message = document.getElementById('message').value;
+//   console.log(message);
+// }
