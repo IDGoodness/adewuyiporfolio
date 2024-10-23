9235fcf4a26acdefd3d6bc7d8a822c57ba6a9999
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-10-23 11:47:20 +0100

    Maybe this resolves the issue of portfolio image 1 not displaying in live production. I just changed the file extension to be capital letters, just like it is in the file.

--- script.js
+++ script.js
@@ -21,11 +21,11 @@
 
 
 
-const getData = () => {
-  const name = document.getElementById('name').value;
-  console.log(name);
-  const email = document.getElementById('email').value;
-  console.log(email);
-  const message = document.getElementById('message').value;
-  console.log(message);
-}
+// const getData = () => {
+//   const name = document.getElementById('name').value;
+//   console.log(name);
+//   const email = document.getElementById('email').value;
+//   console.log(email);
+//   const message = document.getElementById('message').value;
+//   console.log(message);
+// }
