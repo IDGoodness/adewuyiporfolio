4c8f466c31ca677b82e5ae97e3eb784f2a799c5e
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-10-25 12:49:16 +0100

    Email.js just doesn't seem to work, I'm using mailto now. This opens the mail app to send an email.

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-function sendMail() {
-  const params = {
-    from_name: document.getElementById('name').value,
-    email_id: document.getElementById('email').value,
-    message: document.getElementById('message').value
-  };
-  emailjs.send('service_8g4i7pj', 'template_t4m2l8u', params)
-    .then(function(res) {
-      alert("Success!")
-      console.log('SUCCESS!', res.status, res.text);
-    }, function(error) {
-      console.log('FAILED...', error);
-      alert("Failed....!")
-    });
-}
+// // Ensure EmailJS is initialized
+// document.addEventListener("DOMContentLoaded", function() {
+//   emailjs.init("SwvzBam1DXaKItXKm");
+// });
 
-// Ensure EmailJS is initialized
-document.addEventListener("DOMContentLoaded", function() {
-  emailjs.init("YC1scBQUcW0aPNV-M");
-});
+// function sendMail() {
+//   const params = {
+//     from_name: document.getElementById('name').value,
+//     email_id: document.getElementById('email').value,
+//     message: document.getElementById('message').value,
+//   };
+
+//   emailjs.send('service_8g4i7pj', 'template_t4m2l8u', params).then(function() {
+//     alert("Email Sent!");
+//   });
+    
+  
+  
+  
+//   // .then(function(res) {
+//     //   alert("Success!")
+//     //   console.log('SUCCESS!', res.status, res.text);
+//     // }, function(error) {
+//     //   console.log('FAILED...', error);
+//     //   alert("Failed....!")
+//     // });
+// }
